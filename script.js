62100f3915f36fd7b1d75517d203ea2357b4ec16
angelina-xie <xiea867@newschool.edu>
2025-04-21 19:40:19 -0400

    1.5

--- script.js
+++ script.js
@@ -29,71 +29,16 @@ function redirectToPage(event) {
     event.preventDefault();  
     const code = document.getElementById('codetype').value.trim().toLowerCase();  
 
-    if (code === 'rcoai') {
-        window.location.href = 'rcoai.html';
-    } else if (code === 'rcoan') {
-        window.location.href = 'rcoan.html';
-    } else if (code === 'rloan') {
-        window.location.href = 'rloan.html';
-    } else if (code === 'rloai') {
-        window.location.href = 'rloai.html';
-    } else if (code === 'rluai') {
-        window.location.href = 'rluai.html';
-    } else if (code === 'rluan') {
-        window.location.href = 'rluan.html';
-    } else if (code === 'rcuan') {
-        window.location.href = 'rcuan.html';
-    } else if (code === 'rcuai') {
-        window.location.href = 'rcuai.html';
-    } else if (code === 'rcoei') {
-        window.location.href = 'rcoei.html';
-    } else if (code === 'rcoen') {
-        window.location.href = 'rcoen.html';
-    } else if (code === 'rloen') {
-        window.location.href = 'rloen.html';
-    } else if (code === 'rloei') {
-        window.location.href = 'rloei.html';
-    } else if (code === 'rluei') {
-        window.location.href = 'rluei.html';
-    } else if (code === 'rluen') {
-        window.location.href = 'rluen.html';
-    } else if (code === 'rcuen') {
-        window.location.href = 'rcuen.html';
-    } else if (code === 'rcuei') {
-        window.location.href = 'rcuei.html';
-    } else if (code === 'scoai') {
-        window.location.href = 'scoai.html';
-    } else if (code === 'scoan') {
-        window.location.href = 'scoan.html';
-    } else if (code === 'sloan') {
-        window.location.href = 'sloan.html';
-    } else if (code === 'sloai') {
-        window.location.href = 'sloai.html';
-    } else if (code === 'sluai') {
-        window.location.href = 'sluai.html';
-    } else if (code === 'sluan') {
-        window.location.href = 'sluan.html';
-    } else if (code === 'scuan') {
-        window.location.href = 'scuan.html';
-    } else if (code === 'scuai') {
-        window.location.href = 'scuai.html';
-    } else if (code === 'scoei') {
-        window.location.href = 'scoei.html';
-    } else if (code === 'scoen') {
-        window.location.href = 'scoen.html';
-    } else if (code === 'sloen') {
-        window.location.href = 'sloen.html';
-    } else if (code === 'sloei') {
-        window.location.href = 'sloei.html';
-    } else if (code === 'sluei') {
-        window.location.href = 'sluei.html';
-    } else if (code === 'sluen') {
-        window.location.href = 'sluen.html';
-    } else if (code === 'scuen') {
-        window.location.href = 'scuen.html';
-    } else if (code === 'scuei') {
-        window.location.href = 'scuei.html';
+    const redirectMap = {
+      'rcoai': 'rcoai.html', 'rcoan': 'rcoan.html', 'rloan': 'rloan.html', 'rloai': 'rloai.html', 'rluai': 'rluai.html',
+      'rluan': 'rluan.html', 'rcuan': 'rcuan.html', 'rcuai': 'rcuai.html', 'rcoei': 'rcoei.html', 'rcoen': 'rcoen.html',
+      'rloen': 'rloen.html', 'rloei': 'rloei.html', 'rluei': 'rluei.html', 'rluen': 'rluen.html', 'rcuen': 'rcuen.html',
+      'rcuei': 'rcuei.html'
+    };
+  
+    if (redirectMap[code]) {
+      window.location.href = redirectMap[code];
     } else {
-        alert("Oops! That's not a code. Double check spelling?");
+      alert("Invalid code. Maybe you mistyped?");
     }
 }
